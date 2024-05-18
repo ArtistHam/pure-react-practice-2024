c03c2b07ff4c98b57441f88cf6d453fd82614f75
Artist149 <great.artist.ham@gmail.com>
2024-05-18 18:00:12 +0400

    lesson 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import * as styles from "./App.module.css";
 
 const App = () => {
+  const [chosen, setChosen] = useState([]);
+  const [opened, setOpened] = useState([]);
   const [cards, setCards] = useState([
     1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8,
   ]);
@@ -9,6 +11,20 @@ const App = () => {
   useEffect(() => {
     shuffleArray(cards);
   }, []);
+
+  useEffect(() => {
+    if (chosen.length === 2) {
+      if (chosen[0] !== chosen[1]) {
+        const temp = [...opened];
+        temp.pop();
+        temp.pop();
+        setTimeout(() => {
+          setOpened(temp);
+        }, 500);
+      }
+      setChosen([]);
+    }
+  }, [chosen]);
 
   function shuffleArray(array) {
     const copy = [...array];
@@ -21,15 +37,34 @@ const App = () => {
     setCards(copy);
   }
 
+  const handleCardClick = (id) => {
+    setOpened([...opened, id]);
+    setChosen([...chosen, cards[id]]);
+  };
+
   return (
     <div className={styles.page}>
       <div className={styles.header}>
         <h1>Doggo Game</h1>
       </div>
       <div className={styles.field}>
-        {cards.map((cardId) => (
-          <div className={styles.card}>
-            <img src={`./static/images/${cardId}.png`} alt="shiba-inu" />
+        {cards.map((cardId, id) => (
+          <div
+            className={`${styles.card} ${
+              opened.findIndex((openId) => openId === id) !== -1
+                ? styles.chosen
+                : ""
+            }`}
+            onClick={() => {
+              handleCardClick(id);
+            }}
+          >
+            <div className={styles.cardContent}>
+              <div className={styles.cardFront}></div>
+              <div className={styles.cardBack}>
+                <img src={`./static/images/${cardId}.png`} alt="shiba-inu" />
+              </div>
+            </div>
           </div>
         ))}
       </div>
